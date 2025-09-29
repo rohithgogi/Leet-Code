2e298223c8aae610d523025b59c95a866fd4e70d
G. Redy Rohith <111967897+rohithgogi@users.noreply.github.com>
2025-09-29 22:09:01 +0530

    Time: 7 ms (44.31%), Space: 46 MB (20.72%) - LeetHub

---
+++ 0207-course-schedule/0207-course-schedule.java
@@ -0,0 +1,34 @@
+class Solution {
+    public boolean canFinish(int numCourses, int[][] prerequisites) {
+        List<List<Integer>> adj=new ArrayList<>();
+        for(int i=0;i<numCourses;i++){
+            adj.add(new ArrayList<>());
+        }
+        for(int[] it:prerequisites){
+            adj.get(it[0]).add(it[1]);
+        }
+
+        int[] indegree=new int[numCourses];
+        for(int i=0;i<numCourses;i++){
+            for(int it:adj.get(i)){
+                indegree[it]++;
+            }
+        }
+
+        Queue<Integer> q=new LinkedList<>();
+        for(int i=0;i<numCourses;i++){
+            if(indegree[i]==0) q.add(i);
+        }
+        List<Integer> topo=new ArrayList<>();
+        while(!q.isEmpty()){
+            int node=q.poll();
+            topo.add(node);
+            for(int it:adj.get(node)){
+                indegree[it]--;
+                if(indegree[it]==0) q.add(it);
+            }
+        }
+        if(topo.size()==numCourses) return true;
+        return false;
+    }
+}
