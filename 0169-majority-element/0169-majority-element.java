16db97104026102f2bd790631ffc4785bfffdcab
G. Redy Rohith <111967897+rohithgogi@users.noreply.github.com>
2026-03-05 18:12:54 +0530

    Time: 15 ms (22.01%), Space: 52.5 MB (94.88%) - LeetHub

--- 0169-majority-element/0169-majority-element.java
+++ 0169-majority-element/0169-majority-element.java
@@ -1,6 +1,6 @@
 class Solution {
     public int majorityElement(int[] nums) {
-        HashMap<Integer,Integer> map=new HashMap<>();
+        Map<Integer,Integer> map=new HashMap<>();
         for(int i:nums){
             if(map.containsKey(i)){
                 map.put(i,map.get(i)+1);
@@ -8,13 +8,11 @@ class Solution {
                 map.put(i,1);
             }
         }
-        int res=0;
-        for(int key:map.keySet()){
-            if(map.get(key)>nums.length/2){
-                res=key;
+        for(int i:map.keySet()){
+            if(map.get(i)>nums.length/2){
+                return i;
             }
         }
-        return res;
-        
+        return -1;
     }
 }
