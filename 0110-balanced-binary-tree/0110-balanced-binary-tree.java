f642435bd58187b74ffb627528f6831b5f7172b3
G. Redy Rohith <111967897+rohithgogi@users.noreply.github.com>
2026-03-13 15:34:59 +0530

    Time: 0 ms (100%), Space: 45.6 MB (59.77%) - LeetHub

--- 0110-balanced-binary-tree/0110-balanced-binary-tree.java
+++ 0110-balanced-binary-tree/0110-balanced-binary-tree.java
@@ -21,9 +21,8 @@ class Solution {
         if(node==null) return 0;
         int lh=checkBalanced(node.left);
         int rh=checkBalanced(node.right);
-        if(lh==-1 ||rh == -1) return -1;
+        if(lh==-1 || rh==-1) return -1;
         if(Math.abs(lh-rh)>1) return -1;
-
-        return 1+Math.max(lh,rh);
+        return 1+Math.max(lh,rh); 
     }
 }
