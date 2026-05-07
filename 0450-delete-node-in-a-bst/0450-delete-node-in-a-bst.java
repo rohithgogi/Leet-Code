ecfaa1375a887ff2e5135e9f4586a8327f5fd48c
G. Redy Rohith <111967897+rohithgogi@users.noreply.github.com>
2026-05-07 21:12:15 +0530

    Time: 0 ms (100%), Space: 47.4 MB (75.5%) - LeetHub

--- 0450-delete-node-in-a-bst/0450-delete-node-in-a-bst.java
+++ 0450-delete-node-in-a-bst/0450-delete-node-in-a-bst.java
@@ -1,52 +1,60 @@
+/**
+ * Definition for a binary tree node.
+ * public class TreeNode {
+ *     int val;
+ *     TreeNode left;
+ *     TreeNode right;
+ *     TreeNode() {}
+ *     TreeNode(int val) { this.val = val; }
+ *     TreeNode(int val, TreeNode left, TreeNode right) {
+ *         this.val = val;
+ *         this.left = left;
+ *         this.right = right;
+ *     }
+ * }
+ */
 class Solution {
     public TreeNode deleteNode(TreeNode root, int key) {
-        if (root == null) return null;
-
-        // If root itself needs to be deleted
-        if (root.val == key) {
-            return helper(root);
-        }
-
-        TreeNode curr = root;
-
-        while (curr != null) {
-            if (key < curr.val) {
-                if (curr.left != null && curr.left.val == key) {
-                    curr.left = helper(curr.left);
+        if(root==null) return null;
+        if(root.val==key) return helper(root);
+        TreeNode dummy=root;
+        while(root!=null){
+            if(key<root.val){
+                if(root.left!=null && root.left.val==key){
+                    root.left=helper(root.left);
                     break;
-                } else {
-                    curr = curr.left;
+                }else{
+                    root=root.left;
                 }
-            } else {
-                if (curr.right != null && curr.right.val == key) {
-                    curr.right = helper(curr.right);
+            }else{
+                if(root.right!=null && root.right.val==key){
+                    root.right=helper(root.right);
                     break;
-                } else {
-                    curr = curr.right;
+                }else{
+                    root=root.right;
                 }
             }
+            
         }
-
-        return root;
+        return dummy;
     }
 
-    // Handles deletion logic
-    public TreeNode helper(TreeNode root) {
-        if (root.left == null) {
+    public TreeNode helper(TreeNode root){
+        if(root.left==null){
             return root.right;
-        } else if (root.right == null) {
+        }else if(root.right==null){
             return root.left;
-        } else {
-            TreeNode rightChild = root.right;
-            TreeNode lastRight = findLastRight(root.left);
-            lastRight.right = rightChild;
+        }else{
+            TreeNode rightChild=root.right;
+            TreeNode lastRight=findLastRight(root.left);
+            lastRight.right=rightChild;
             return root.left;
         }
     }
-
-    // Finds rightmost node in left subtree
-    public TreeNode findLastRight(TreeNode root) {
-        if (root.right == null) return root;
+    public TreeNode findLastRight(TreeNode root){
+        if(root.right==null){
+            return root;
+        }
         return findLastRight(root.right);
     }
 }
