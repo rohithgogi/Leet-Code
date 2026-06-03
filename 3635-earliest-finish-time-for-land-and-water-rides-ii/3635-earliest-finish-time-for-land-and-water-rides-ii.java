7687098e3747378fd169155c70062eb6a440f5fa
G. Redy Rohith <111967897+rohithgogi@users.noreply.github.com>
2026-06-03 17:56:23 +0530

    Time: 2 ms (100%), Space: 91.9 MB (63.79%) - LeetHub

--- 3635-earliest-finish-time-for-land-and-water-rides-ii/3635-earliest-finish-time-for-land-and-water-rides-ii.java
+++ 3635-earliest-finish-time-for-land-and-water-rides-ii/3635-earliest-finish-time-for-land-and-water-rides-ii.java
@@ -1,112 +1,23 @@
-import java.util.*;
-
 class Solution {
+    public int earliestFinishTime(int[] landStartTime, int[] landDuration, int[] waterStartTime, int[] waterDuration) {
+        int n=landDuration.length;
+        int m=waterDuration.length;
+        int a1=Integer.MAX_VALUE;
+        int a2=Integer.MAX_VALUE;
 
-    public int earliestFinishTime(int[] landStartTime, int[] landDuration,
-                                  int[] waterStartTime, int[] waterDuration) {
-
-        int ans1 = solveOrder(
-                landStartTime,
-                landDuration,
-                waterStartTime,
-                waterDuration
-        );
-
-        int ans2 = solveOrder(
-                waterStartTime,
-                waterDuration,
-                landStartTime,
-                landDuration
-        );
-
-        return Math.min(ans1, ans2);
-    }
-
-    private int solveOrder(int[] firstStart, int[] firstDuration,
-                           int[] secondStart, int[] secondDuration) {
-
-        int m = secondStart.length;
-
-        int[][] rides = new int[m][2];
-
-        for (int i = 0; i < m; i++) {
-            rides[i][0] = secondStart[i];
-            rides[i][1] = secondDuration[i];
+        for(int i=0;i<n;i++){
+            a1=Math.min(a1,landStartTime[i]+landDuration[i]);  
         }
-
-        Arrays.sort(rides, (a, b) -> Integer.compare(a[0], b[0]));
-
-        int[] starts = new int[m];
-        int[] prefMinDuration = new int[m];
-        int[] suffMinFinish = new int[m];
-
-        for (int i = 0; i < m; i++) {
-            starts[i] = rides[i][0];
+        for(int i=0;i<m;i++){
+            a2=Math.min(a2,waterStartTime[i]+waterDuration[i]);  
         }
-
-        prefMinDuration[0] = rides[0][1];
-        for (int i = 1; i < m; i++) {
-            prefMinDuration[i] =
-                    Math.min(prefMinDuration[i - 1], rides[i][1]);
+        int ans=Integer.MAX_VALUE;
+        for(int i=0;i<n;i++){
+            ans = Math.min(ans,Math.max(a2, landStartTime[i]) + landDuration[i]);
         }
-
-        suffMinFinish[m - 1] =
-                rides[m - 1][0] + rides[m - 1][1];
-
-        for (int i = m - 2; i >= 0; i--) {
-            suffMinFinish[i] =
-                    Math.min(
-                            suffMinFinish[i + 1],
-                            rides[i][0] + rides[i][1]
-                    );
+        for(int i=0;i<m;i++){
+            ans = Math.min(ans,Math.max(a1, waterStartTime[i]) + waterDuration[i]);
         }
-
-        int answer = Integer.MAX_VALUE;
-
-        for (int i = 0; i < firstStart.length; i++) {
-
-            int endTime = firstStart[i] + firstDuration[i];
-
-            int idx = upperBound(starts, endTime);
-
-            // Case 1:
-            // second ride starts before or exactly at endTime
-            if (idx > 0) {
-                answer = Math.min(
-                        answer,
-                        endTime + prefMinDuration[idx - 1]
-                );
-            }
-
-            // Case 2:
-            // second ride starts after endTime
-            if (idx < m) {
-                answer = Math.min(
-                        answer,
-                        suffMinFinish[idx]
-                );
-            }
-        }
-
-        return answer;
-    }
-
-    private int upperBound(int[] arr, int target) {
-
-        int left = 0;
-        int right = arr.length;
-
-        while (left < right) {
-
-            int mid = left + (right - left) / 2;
-
-            if (arr[mid] <= target) {
-                left = mid + 1;
-            } else {
-                right = mid;
-            }
-        }
-
-        return left;
+        return ans;
     }
 }
