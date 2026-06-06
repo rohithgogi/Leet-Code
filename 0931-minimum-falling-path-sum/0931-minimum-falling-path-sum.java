e3c72ed8c1e3c9f60da6a70417bc3de4f28cbae0
G. Redy Rohith <111967897+rohithgogi@users.noreply.github.com>
2026-06-06 16:15:43 +0530

    Time: 12 ms (5.69%), Space: 47.2 MB (46.96%) - LeetHub

--- 0931-minimum-falling-path-sum/0931-minimum-falling-path-sum.java
+++ 0931-minimum-falling-path-sum/0931-minimum-falling-path-sum.java
@@ -3,10 +3,26 @@ class Solution {
         int n=matrix.length;
         int ans=Integer.MAX_VALUE;
         Integer[][] dp=new Integer[n][n];
+
         for(int i=0;i<n;i++){
-            int curr=minPath(matrix,0,i,dp);
-            ans=Math.min(curr,ans);
+            dp[n-1][i]=matrix[n-1][i];
         }
+        for(int i=n-2;i>=0;i--){
+            for(int j=0;j<=n-1;j++){
+                int down=dp[i+1][j];
+                int left=(j-1>=0)?dp[i+1][j-1]:(int) 1e9;
+                int right=(j+1)<n?dp[i+1][j+1]:(int) 1e9;
+                dp[i][j]=matrix[i][j]+Math.min(Math.min(left,right),down);
+            }
+        }
+        for(int i:dp[0]){
+            ans=Math.min(ans,i);
+        }
+        
+        // for(int i=0;i<n;i++){
+        //     int curr=minPath(matrix,0,i,dp);
+        //     ans=Math.min(curr,ans);
+        // }
         return ans;
 
     }
