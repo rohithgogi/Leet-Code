94cb6b67a649aaf3e82d7d997744cee56a236cd8
G. Redy Rohith <111967897+rohithgogi@users.noreply.github.com>
2026-06-22 19:36:16 +0530

    Time: 6 ms (55.31%), Space: 43.9 MB (36.65%) - LeetHub

---
+++ 1189-maximum-number-of-balloons/1189-maximum-number-of-balloons.java
@@ -0,0 +1,23 @@
+class Solution {
+    public int maxNumberOfBalloons(String text) {
+        Map<Character,Integer> map=new HashMap<>();
+        for(char c:text.toCharArray()){
+            if(c=='b'||c=='a' ||c=='l' ||c=='o' ||c=='n'){
+                map.put(c,map.getOrDefault(c,0)+1);
+            }
+        }
+        int count=0;
+        boolean flag=true;
+        while(flag){
+            for(char c: "balloon".toCharArray()){
+                if(!map.containsKey(c) || map.get(c)==0){
+                    flag=false;
+                    break;
+                }
+                map.put(c,map.get(c)-1);
+            }
+            count++;
+        }
+        return count-1;
+    }
+}
