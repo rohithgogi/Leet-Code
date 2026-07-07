2f2015b5e5595b1cb9d3a3de6e27e290c6d1aedc
G. Redy Rohith <111967897+rohithgogi@users.noreply.github.com>
2026-07-07 16:17:41 +0530

    Time: 693 ms (18.54%), Space: 0B (100%) - LeetHub

---
+++ 1164-product-price-at-a-given-date/1164-product-price-at-a-given-date.sql
@@ -0,0 +1,12 @@
+# Write your MySQL query statement below
+SELECT p.product_id,
+    COALESCE(
+        (SELECT new_price
+        FROM products p2
+        WHERE p2.product_id=p.product_id
+        AND p2.change_date<='2019-08-16'
+        ORDER BY change_date DESC
+        LIMIT 1),
+        10
+    )AS price
+FROM (SELECT DISTINCT product_id FROM products) p;
