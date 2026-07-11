f64fb040cfc8d390083dc0c124a46238434a375d
G. Redy Rohith <111967897+rohithgogi@users.noreply.github.com>
2026-07-11 19:48:07 +0530

    Time: 2 ms (91.19%), Space: 44.2 MB (40.46%) - LeetHub

--- 0994-rotting-oranges/0994-rotting-oranges.java
+++ 0994-rotting-oranges/0994-rotting-oranges.java
@@ -1,50 +1,59 @@
+class Pair{
+    int row;
+    int col;
+    int tm;
+    public Pair(int row,int col,int tm){
+        this.row=row;
+        this.col=col;
+        this.tm=tm;
+    }
+}
 
 class Solution {
+
     public int orangesRotting(int[][] grid) {
         int n=grid.length;
         int m=grid[0].length;
-        int count=0;
-        Queue<List<Integer>> q=new LinkedList<>();
         int[][] vis=new int[n][m];
-        for(int i=0;i<n;i++){
-            for(int j=0;j<m;j++){
+        int cntFresh=0;
+        Queue<Pair> q=new LinkedList<>();
+        int[] rdir={-1,0,1,0};
+        int[] cdir={0,1,0,-1};
+        for(int i=0;i<grid.length;i++){
+            for(int j=0;j<grid[0].length;j++){
                 if(grid[i][j]==2){
-                    q.offer(Arrays.asList(i,j,0));
+                    q.add(new Pair(i,j,0));
                     vis[i][j]=2;
                 }else{
                     vis[i][j]=0;
                 }
+                if(grid[i][j]==1) cntFresh++;
             }
         }
+
+
         int tm=0;
-        int[] rdir={-1,0,1,0};
-        int[] cdir={0,-1,0,1};
+        int cnt=0;
         while(!q.isEmpty()){
-            int r=q.peek().get(0);
-            int c=q.peek().get(1);
-            int t=q.peek().get(2);
-            tm=Math.max(tm,t);
+            int row=q.peek().row;
+            int col=q.peek().col;
+            int t=q.peek().tm;  
+            tm=Math.max(t,tm);
             q.remove();
+
+
             for(int i=0;i<4;i++){
-                int nrow=r+rdir[i];
-                int ncol=c+cdir[i];
-                if(nrow>=0 && ncol>=0 && nrow<n && ncol<m && vis[nrow][ncol]!=2 && grid[nrow][ncol]==1){
-                    q.offer(Arrays.asList(nrow,ncol,t+1));
-                    vis[nrow][ncol]=2;
+                int nr=row+rdir[i];
+                int nc=col+cdir[i];
+                if(nr>=0 && nc>=0 && nr<n && nc<m && vis[nr][nc]==0 && grid[nr][nc]==1){
+                    q.add(new Pair(nr,nc,t+1));
+                    vis[nr][nc]=2;
+                    cnt++;
                 }
             }
         }
-
-        for(int i=0;i<n;i++){
-            for(int j=0;j<m;j++){
-                if(vis[i][j]!=2 && grid[i][j]==1){
-                    return -1;
-                }
-            }
-        }
+        if(cnt!=cntFresh) return -1;
         return tm;
-
-
     }
-   
+    
 }
