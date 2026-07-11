c2f5fdc21074243f7a9b0af5b27aff6ef1800cb9
G. Redy Rohith <111967897+rohithgogi@users.noreply.github.com>
2026-07-12 00:11:40 +0530

    Time: 3 ms (73.15%), Space: 53.5 MB (68.2%) - LeetHub

---
+++ 0997-find-the-town-judge/0997-find-the-town-judge.java
@@ -0,0 +1,17 @@
+class Solution {
+    public int findJudge(int n, int[][] trust) {
+        int[] inDegree=new int[n+1];
+        int[] outDegree=new int[n+1];
+        for(int[] arr: trust){
+            outDegree[arr[0]]++;
+            inDegree[arr[1]]++;
+        }
+        
+
+        for(int i=1;i<=n;i++){
+            if(inDegree[i]==n-1 && outDegree[i]==0) return i;
+        }
+        return -1;
+    }
+    
+}
