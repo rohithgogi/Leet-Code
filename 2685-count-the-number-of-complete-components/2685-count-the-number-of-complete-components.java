ec79830a805ac3044c401869a566749712a7e2b4
G. Redy Rohith <111967897+rohithgogi@users.noreply.github.com>
2026-07-11 11:53:04 +0530

    Time: 11 ms (59.6%), Space: 47.7 MB (39.49%) - LeetHub

--- 2685-count-the-number-of-complete-components/2685-count-the-number-of-complete-components.java
+++ 2685-count-the-number-of-complete-components/2685-count-the-number-of-complete-components.java
@@ -6,12 +6,10 @@ class Solution {
         for(int i=0;i<n;i++){
             adj.add(new ArrayList<Integer>());
         }
-
         for(int[] e:edges){
             adj.get(e[0]).add(e[1]);
             adj.get(e[1]).add(e[0]);
         }
-
         for(int i=0;i<n;i++){
             if(!vis[i]){
                 int[] res=dfs(i,adj,vis);
