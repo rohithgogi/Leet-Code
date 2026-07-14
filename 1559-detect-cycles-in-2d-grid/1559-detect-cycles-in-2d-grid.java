1081591eb1a83db7db59b373ef62a639ede556d5
G. Redy Rohith <111967897+rohithgogi@users.noreply.github.com>
2026-07-14 16:16:08 +0530

    Time: 18 ms (77.26%), Space: 138 MB (69.87%) - LeetHub

--- 1559-detect-cycles-in-2d-grid/1559-detect-cycles-in-2d-grid.java
+++ 1559-detect-cycles-in-2d-grid/1559-detect-cycles-in-2d-grid.java
@@ -1,31 +1,35 @@
 class Solution {
+    private int[] rdir={0,1,0,-1};
+    private int[] cdir={1,0,-1,0};
+
+    
     public boolean containsCycle(char[][] grid) {
         boolean[][] vis=new boolean[grid.length][grid[0].length];
         for(int i=0;i<grid.length;i++){
             for(int j=0;j<grid[0].length;j++){
                 if(!vis[i][j]){
-                    if(dfs(i,j,-1,-1,grid,vis)) return true;;
+                    if(dfs(i,j,-1,-1,vis,grid)) return true;  
                 }
             }
         }
         return false;
     }
-    public boolean dfs(int r,int c, int pr,int pc, char[][] grid, boolean[][] vis){
-        vis[r][c]=true;
-        int rdir[]={0,-1,0,1};
-        int cdir[]={-1,0,1,0};
-
+    public boolean dfs(int row,int col,int pr,int pc,boolean[][] vis,char[][] grid){
+        
+        vis[row][col]=true;
         for(int i=0;i<4;i++){
-            int nr=r+rdir[i];
-            int nc=c+cdir[i];
-            if(nr>=0 && nr<grid.length && nc>=0 && nc<grid[0].length && grid[r][c]==grid[nr][nc] ){
+            int nr=row+rdir[i];
+            int nc=col+cdir[i];
+            if(nr>=0 && nr<grid.length && nc>=0 && nc<grid[0].length && grid[nr][nc]==grid[row][col]){
                 if(!vis[nr][nc]){
-                    if(dfs(nr,nc,r,c,grid,vis)) return true;
-                }else if((nr!=pr) && (nc!=pc)){
+                    if( dfs(nr,nc,row,col,vis,grid)) return true;
+                }else if(nr!=pr && nc!=pc){
                     return true;
                 }
+               
             }
         }
         return false;
+
     }
 }
