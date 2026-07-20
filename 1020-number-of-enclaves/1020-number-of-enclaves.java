8e032980692651870e08a873ad7af2b45b3b39c4
G. Redy Rohith <111967897+rohithgogi@users.noreply.github.com>
2026-07-20 15:03:10 +0530

    Time: 11 ms (14.88%), Space: 54.7 MB (79.27%) - LeetHub

--- 1020-number-of-enclaves/1020-number-of-enclaves.java
+++ 1020-number-of-enclaves/1020-number-of-enclaves.java
@@ -1,51 +1,65 @@
+class Node{
+    int row;
+    int col;
+    Node(int row,int col){
+        this.row=row;
+        this.col=col;
+    }
+}
+
 class Solution {
+    private int[] rdir={1,0,-1,0};
+    private int[] cdir={0,-1,0,1};
     public int numEnclaves(int[][] grid) {
-        int n=grid.length;
-        int m=grid[0].length;
-        boolean[][] vis=new boolean[n][m];
+        int m=grid.length;
+        int n=grid[0].length;
+        int[][] vis=new int[m][n];
+        Queue<Node> q=new LinkedList<>();
+
+        for(int i=0;i<m;i++){
+            if(grid[i][0]==1 && vis[i][0]==0){
+                vis[i][0]=1;
+                q.add(new Node(i,0));
+            }
+            if(grid[i][n-1]==1 && vis[i][n-1]==0){
+                vis[i][n-1]=1;
+                q.add(new Node(i,n-1));
+            }
+        }
+        for(int i=0;i<n;i++){
+            if(grid[0][i]==1 && vis[0][i]==0){
+                vis[0][i]=1;
+                q.add(new Node(0,i));
+            }
+            if(grid[m-1][i]==1 && vis[m-1][i]==0){
+                vis[m-1][i]=1;
+                q.add(new Node(m-1,i));
+            }
+        }
+
+        while(!q.isEmpty()){
+            int row=q.peek().row;
+            int col=q.peek().col;
+            q.remove();
+            
+            for(int i=0;i<4;i++){
+                int nr=row+rdir[i];
+                int nc=col+cdir[i];
+                if(nr>=0 && nr<m && nc>=0 && nc<n && grid[nr][nc]==1 && vis[nr][nc]==0){
+                    vis[nr][nc]=1;
+                    q.add(new Node(nr,nc));
+                    
+                }
+            }
+        }
         int count=0;
-        for(int j=0;j<m;j++){
-            if(!vis[0][j] && grid[0][j]==1){
-                dfs(grid,vis,0,j);
-            }
-            
-        }
-        for(int i=0;i<n;i++){
-            if(!vis[i][m-1] && grid[i][m-1]==1){
-                dfs(grid,vis,i,m-1);
-            }
-            
-        }
-        for(int j=m-1;j>=0;j--){
-            if(!vis[n-1][j] && grid[n-1][j]==1){
-                dfs(grid,vis,n-1,j);
-            }
-        }
-        for(int i=n-1;i>=0;i--){
-            if(!vis[i][0] && grid[i][0]==1){
-                dfs(grid,vis,i,0);
-            }
-        }
-        for(int i=0;i<n;i++){
-            for(int j=0;j<m;j++){
-                if(vis[i][j]==false && grid[i][j]==1){
+        for(int i=0;i<m;i++){
+            for(int j=0;j<n;j++){
+                if(vis[i][j]==0 && grid[i][j]==1){
                     count++;
                 }
             }
         }
         return count;
-        
-    }
-    public void dfs(int[][] grid,boolean[][] vis,int r,int c){
-        vis[r][c]=true;
-        int[] rdir={0,-1,0,1};
-        int[] cdir={1,0,-1,0};
-        for(int i=0;i<4;i++){
-            int nr=r+rdir[i];
-            int nc=c+cdir[i];
-            if(nr>=0 && nr<grid.length && nc>=0 && nc<grid[0].length && grid[nr][nc]==1 && !vis[nr][nc]){
-                dfs(grid,vis,nr,nc);
-            }
-        }
     }
 }
