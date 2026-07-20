06004202b1921adb3a751a8c47d599e2f7b0ac60
G. Redy Rohith <111967897+rohithgogi@users.noreply.github.com>
2026-07-20 11:59:20 +0530

    Time: 16 ms (27.33%), Space: 49.7 MB (15.64%) - LeetHub

--- 0542-01-matrix/0542-01-matrix.java
+++ 0542-01-matrix/0542-01-matrix.java
@@ -1,38 +1,57 @@
+
+class Node{
+    int first;
+    int second;
+    int third;
+    Node(int first,int second,int third){
+        this.first=first;
+        this.second=second;
+        this.third=third;
+    }
+}
 class Solution {
+    private int[] rdir={0,1,0,-1};
+    private int[] cdir={1,0,-1,0};
+    
     public int[][] updateMatrix(int[][] mat) {
-        int[][] dis=new int[mat.length][mat[0].length];
-        boolean[][] vis=new int[mat.length][mat[0].length];
-        Queue<int[]> q=new LinkedList<>();
-        for(int i=0;i<mat.length;i++){
-            for(int j=0;j<mat[0].length;j++){
+        int n=mat.length;
+        int m=mat[0].length;
+        int[][] vis=new int[n][m];
+        int[][] dist=new int[n][m];
+
+        Queue<Node> q=new LinkedList<>();
+
+        for(int i=0;i<n;i++){
+            for(int j=0;j<m;j++){
                 if(mat[i][j]==0){
-                    q.add(new int[]{i,j,0});
-                    vis[i][j]=true;
+                    q.add(new Node(i,j,0));
+                    vis[i][j]=1;
                 }else{
-                    vis[i][j]=false;
+                    vis[i][j]=0;
                 }
             }
         }
-        int[] rdir={0,-1,0,1};
-        int[] cdir={1,0,-1,0};
 
         while(!q.isEmpty()){
-            int[] arr=q.poll();
-            int row=arr[0];
-            int col=arr[1];
-            int d=arr[2];
-            dis[row][col]=d;
+            int row=q.peek().first;
+            int col=q.peek().second;
+            int steps=q.peek().third;
+            q.remove();
+            dist[row][col]=steps;
             for(int i=0;i<4;i++){
-                int r=row+rdir[i];
-                int c=col+cdir[i];
+                int nr=row+rdir[i];
+                int nc=col+cdir[i];
 
-                if(r>=0 && r<mat.length && c>=0 && c<mat[0].length && !vis[r][c]){
-                    vis[r][c]=true;
-                    q.add(new int[]{r,c,d+1});
+                if(nr>=0 && nc>=0 &&nr<n && nc<m &&vis[nr][nc]==0){
+                    vis[nr][nc]=1;
+                    q.add(new Node(nr,nc,steps+1));
                 }
             }
         }
-        return dis;
 
+
+        return dist;
     }
+    
+
 }
