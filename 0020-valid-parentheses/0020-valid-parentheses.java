2969b1a92452f3069857da377d535e6ac6ddd19c
G. Redy Rohith <111967897+rohithgogi@users.noreply.github.com>
2026-08-03 19:11:07 +0530

    Time: 5 ms (13.29%), Space: 43.5 MB (15.52%) - LeetHub

--- 0020-valid-parentheses/0020-valid-parentheses.java
+++ 0020-valid-parentheses/0020-valid-parentheses.java
@@ -1,24 +1,26 @@
-import java.util.*;
-
 class Solution {
     public boolean isValid(String s) {
-        if (s.length() % 2 != 0) return false;
-
-        Stack<Character> stack = new Stack<>();
-        
-        for (char c : s.toCharArray()) {
-            if (c == '(' || c == '[' || c == '{') {
-                stack.push(c);
-            } else {
-                if (stack.isEmpty()) return false;
-                
-                char top = stack.peek();
-                if ((c == ')' && top == '(') ||
-                    (c == ']' && top == '[') ||
-                    (c == '}' && top == '{')) {
+        Map<Character,Character> map=new HashMap<>();
+        map.put('(',')');
+        map.put('{','}');
+        map.put('[',']');
+        Stack<Character> stack=new Stack<>();
+        for(char c:s.toCharArray()){
+            if(!stack.isEmpty()){
+                char prev=stack.peek();
+                if(map.get(prev)==c){
                     stack.pop();
-                } else {
+                }else if(c==')' || c==']' || c=='}'){
                     return false;
+                }else{
+                    stack.push(c);
+                }
+
+            }else{
+                if(c==')' || c==']' || c=='}'){
+                    return false;
+                } else{
+                    stack.push(c);
                 }
             }
         }
