7fe890af61f31fd139daf78994e2bf9f62194708
G. Redy Rohith <111967897+rohithgogi@users.noreply.github.com>
2026-08-18 13:15:46 +0530

    Time: 182 ms (5.08%), Space: 65 MB (12.32%) - LeetHub

--- 0802-find-eventual-safe-states/0802-find-eventual-safe-states.java
+++ 0802-find-eventual-safe-states/0802-find-eventual-safe-states.java
@@ -1,39 +1,37 @@
 class Solution {
     public List<Integer> eventualSafeNodes(int[][] graph) {
-        int n=graph.length;
-        boolean[] vis=new boolean[n];
-        boolean[] pathvis=new boolean[n];
-        List<Integer> safeNode=new ArrayList<>();
-        int[] check=new int[n];
-        for(int i=0;i<n;i++){
-            if(!vis[i]){
-                dfs(i,graph,vis,pathvis,check);
+        int[] vis=new int[graph.length];
+        int[] pathvis=new int[graph.length];
+        int[] check=new int[graph.length];
+        for(int i=0;i<graph.length;i++){
+            if(vis[i]==0){
+                dfsCheck(i,graph,vis,pathvis,check);
             }
         }
-        for(int i=0;i<n;i++){
+        List<Integer> safeNodes=new ArrayList<>();
+        for(int i=0;i<check.length;i++){
             if(check[i]==1){
-                safeNode.add(i);
+                safeNodes.add(i);
             }
         }
-        Collections.sort(safeNode);
-        return safeNode;
+        return safeNodes;
     }
-    private boolean dfs(int node,int[][] graph,boolean[] vis,boolean[] pathvis,int[] check){
-        vis[node]=true;
-        pathvis[node]=true;
+    public boolean dfsCheck(int node,int[][] graph,int[] vis,int[] pathvis,int[] check){
+        vis[node]=1;
+        pathvis[node]=1;
         check[node]=0;
-        for(int it:graph[node]){
-            if(!vis[it]){
-                if(dfs(it,graph,vis,pathvis,check) == true){
-                    check[it]=0;
+
+        for(int i:graph[node]){
+            if(vis[i]==0){
+                if(dfsCheck(i,graph,vis,pathvis,check)==true){
                     return true;
                 }
-            }else if(pathvis[it]==true){
+            }else if(pathvis[i]==1){
                 return true;
             }
         }
         check[node]=1;
-        pathvis[node]=false;
+        pathvis[node]=0;
         return false;
     }
 }
